76d38dc6d4bb6219b132964b79b1ba8b710f08cb
Farelito <bakoufarel@gmail.com>
2024-03-25 15:57:17 +0000

    test

--- outils.c
+++ outils.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include "main.h"
 
 /**
  * _strlen - Prints the length of a string
- * @s: pointer to a string
+ * @str: pointer to a string
  *
  * Description: Print the length of the string
  * Return: length, the size of the string
  */
-int _strlen(char *s)
+int _strlen(const char *str)
 {
 	int length = 0;
 
-	while (*s != '\0')
+	while (str[length] != '\0')
 	{
 		length++;
-		s++;
 	}
 	return (length);
 }
