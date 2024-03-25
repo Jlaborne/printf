07b5e93dbbebe4fd251536dc9d88769bd980847b
Farelito <bakoufarel@gmail.com>
2024-03-25 14:31:50 +0000

    edit percent

--- print_percent.c
+++ print_percent.c
@@ -5,7 +5,10 @@
 int print_percent(char *separator, va_list args)
 {
    
-    int percent_value = va_arg(args, int);
+     int percent_value = va_arg(args, int);
+
+    if (separator != NULL && separator[0] != '\0')
+        printf("%s", separator);
 
     printf("%d%%", percent_value);
 
