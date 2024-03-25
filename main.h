957ef14f1fa8a8ff4fb2f817771aa2bb57c7dee7
Farelito <bakoufarel@gmail.com>
2024-03-25 15:36:04 +0000

    try print_all

--- main.h
+++ main.h
@@ -1,16 +1,7 @@
 #ifndef PRINTF_H
 #define PRINTF_H
+
 #include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-
-int _putchar(char c);
-
-int _printf(const char *format, ...);
-void print_all(const char * const format, ...);
-void print_char(char *separator, va_list args);
-void print_string(char *separator, va_list args);
-void print_percent(char *separator, va_list args);
 
 int _strlen(const char *s);
 /**
@@ -24,5 +15,12 @@ typedef struct _format
 	char *type;
 	int (*f)(char *separator, va_list args);
 } frmt_id;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void print_all(const char * const format, ...);
+void print_char(char *separator, va_list args);
+void print_string(char *separator, va_list args);
+void print_percent(char *separator, va_list args);
 
 #endif
--- print_all.c
+++ print_all.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * print_all - Print values based on format string
  * @format: The format string
@@ -8,32 +9,32 @@
 void print_all(const char * const format, ...)
 {
     va_list args;
-	f_dt form_types[] = {
+	   unsigned int i = 0;
+		unsigned int j;
+	char *separator = " ";
+	
+	 struct f_dt {
+        const char *identifier;
+        void (*f)(char *, 
+		} va_list);form_types[] = {
 		{ "c", print_char },
 		{ "s", print_string },
         { "%%", print_percent }
 	};
-    unsigned int i = 0;
-	unsigned int j = 0;
-	char *separator = " ";
 
+	
 	va_start(args, format);
 
-	while (format != NULL && format[i])
-	{
-		j = 0;
-		while (j < sizeof(form_types) / sizeof(form_types[0]))
-		{
-			if (format[i] == *form_types[j].identifier)
-			{
-				form_types[j].f(separator, args);
-				separator = ", ";
+	  while (format != NULL && format[i]) {
+        for (j = 0; j < sizeof(form_types) / sizeof(form_types[0]); j++) {
+            if (format[i] == *form_types[j].identifier) {
+                form_types[j].f(separator, args);
+                separator = ", ";
                 break;
-			}
-			j++;
-		}
-		i++;
-	}
+            }
+        }
+        i++;
+    }
 
 	va_end(args);
 	printf("\n");
